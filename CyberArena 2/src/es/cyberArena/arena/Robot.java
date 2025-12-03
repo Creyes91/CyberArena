91f9df81f9fd2b4b44b207e94bc169365fb11fe2
gonzagmmp <gonzagmmp@gmail.com>
2025-12-03 15:58:30 +0100

    Creación clase Robot

---
+++ CyberArena 2/src/es/cyberArena/arena/Robot.java
@@ -0,0 +1,75 @@
+package es.cyberArena.arena;
+
+import java.util.Random;
+
+public class Robot {
+	// Atributos
+	private String nombreRobot;
+	private int vidaRobot = 100;
+	private Arma arma;
+
+	// Constructores
+
+	public Robot(String nombreRobot, Arma arma) {
+
+		this.nombreRobot = nombreRobot;
+		this.arma = arma;
+	}
+
+	public Robot(String nombreRobot) {
+
+		this.nombreRobot = nombreRobot;
+		// this.arma = new Arma (ARMA_DEFECTO, POTENCIA_DEFECTO)
+	}
+
+	// Getters
+
+	public String getNombreRobot() {
+		return nombreRobot;
+	}
+
+	public int getVidaRobot() {
+		return vidaRobot;
+	}
+
+	// Métodos
+
+	public void atacar(Robot robotEnemigo) {
+		int dano = 0;
+		Random random = new Random();
+		int factorSuerte = random.nextInt(8);
+		dano = this.arma.getPotenciaArma() + factorSuerte;
+
+		robotEnemigo.recibirDano(dano);
+
+		System.out.println(this.getNombreRobot() + " ataca ferozmente a " + robotEnemigo.getNombreRobot() + "con"
+				+ arma.getNombreArma());
+
+	}
+
+	public void recibirDano(int cantidadDano) {
+		vidaRobot = vidaRobot - cantidadDano;
+
+		if (vidaRobot < 0) {
+			vidaRobot = 0;
+		}
+
+		System.out.println(
+				this.getNombreRobot() + " recibe " + cantidadDano + " puntos de daño. Vida restante: " + vidaRobot);
+	}
+
+	public boolean estaVivo() {
+
+		if (vidaRobot > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return this.getNombreRobot() + " armado con " + arma.getNombreArma();
+	}
+}
